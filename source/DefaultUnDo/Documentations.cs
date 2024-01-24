e622244e1cb0f42e472941e60643d6fa027d30ca
Laszlo Paillat <laszlo.paillat@gmail.com>
2024-01-24 11:11:27 +0100

    code cleanup

--- source/DefaultUnDo/Documentations.cs
+++ source/DefaultUnDo/Documentations.cs
@@ -3,6 +3,5 @@
     /// <summary>
     /// This is the full API documentation of DefaultUnDo.
     /// </summary>
-    internal static class AssemblyDoc
-    { }
+    internal static class AssemblyDoc;
 }
--- source/DefaultUnDo/Internal/MissingAPIs/StackTraceHiddenAttribute.cs
+++ source/DefaultUnDo/Internal/MissingAPIs/StackTraceHiddenAttribute.cs
@@ -3,11 +3,7 @@
 namespace System.Diagnostics
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Struct, Inherited = false)]
-    internal sealed class StackTraceHiddenAttribute : Attribute
-    {
-        public StackTraceHiddenAttribute()
-        { }
-    }
+    internal sealed class StackTraceHiddenAttribute : Attribute;
 }
 
 #endif
